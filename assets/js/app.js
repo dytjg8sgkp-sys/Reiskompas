d38da2c3f547c8a7166dca1c233982d5f56b919a
dytjg8sgkp-sys <dytjg8sgkp@privaterelay.appleid.com>
2026-07-21 10:08:34 +0200

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,9 @@
 
 async function init(){
  const d=await (await fetch('assets/data/app.json')).json();
- countdown.textContent=d.countdown;
- docs.innerHTML=d.docs.map(x=>'<li>'+x+'</li>').join('');
- plan.innerHTML=d.days.map(r=>`<tr><td>${r[0]}</td><td>${r[1]}</td></tr>`).join('');
+ widgets.innerHTML=d.widgets.map(w=>`<div class='card widget'><strong>${w[0]}</strong><span>${w[1]}</span></div>`).join('');
+ camp.innerHTML=d.places.map(p=>`<div>${p}</div>`).join('');
+ rests.innerHTML=d.restaurants.map(r=>'<li>'+r+'</li>').join('');
+ beaches.innerHTML=d.beaches.map(b=>'<li>'+b+'</li>').join('');
 }
 window.onload=init;
