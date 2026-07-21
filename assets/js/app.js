9459260340faa2c5f9c2f9af48840f0f3499ad26
dytjg8sgkp-sys <dytjg8sgkp@privaterelay.appleid.com>
2026-07-21 10:01:57 +0200

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,21 @@
+
 async function init(){
-const d=await (await fetch('assets/data/trip.json')).json();
-const b=d.booking;
-info.innerHTML=`<table>
-<tr><td>Camping</td><td>${b.camping}</td></tr>
-<tr><td>Type</td><td>${b.type}</td></tr>
-<tr><td>Aankomst</td><td>${b.arrival}</td></tr>
-<tr><td>Vertrek</td><td>${b.departure}</td></tr>
-<tr><td>Nachten</td><td>${b.nights}</td></tr>
-<tr><td>Referentie</td><td>${b.reference}</td></tr>
-<tr><td>Totaal</td><td>${b.price}</td></tr>
-<tr><td>Telefoon</td><td>${b.phone}</td></tr>
-</table>`;
-plan.innerHTML=d.planning.map(x=>`<tr><td>${x[0]}</td><td>${x[1]}</td></tr>`).join('');
+ const d=await (await fetch('assets/data/trip.json')).json();
+ document.getElementById('booking').innerHTML=
+ `<strong>${d.booking.camping}</strong><br>
+ Aankomst: ${d.booking.arrival}<br>
+ Vertrek: ${d.booking.departure}<br>
+ Referentie: ${d.booking.reference}`;
+ document.getElementById('route').innerHTML=d.route.map(x=>'<li>'+x+'</li>').join('');
+ document.getElementById('rests').innerHTML=d.favorites.restaurants.map(x=>'<li>'+x+'</li>').join('');
+ document.getElementById('beaches').innerHTML=d.favorites.beaches.map(x=>'<li>'+x+'</li>').join('');
+ const names=["Receptie","Zwembad","Restaurant","Supermarkt","Sanitair","Sport","Speeltuin","Jullie plaats"];
+ const map=document.getElementById('map');
+ names.forEach((n,i)=>{
+   const div=document.createElement('div');
+   div.className='tile'+(i===7?' active':'');
+   div.textContent=n;
+   map.appendChild(div);
+ });
 }
 window.onload=init;
