fd168f1903b020cba847245c7f5e7a9b2f6158e2
dytjg8sgkp-sys <dytjg8sgkp@privaterelay.appleid.com>
2026-07-21 09:53:31 +0200

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,11 @@
 
 async function init(){
- const d=await (await fetch('assets/data/data.json')).json();
- const st=JSON.parse(localStorage.getItem('rk71')||'{}');
- const days=document.getElementById('days');
- d.days.forEach((day,di)=>{
-   const div=document.createElement('div');
-   div.innerHTML=`<h3>${day.title}</h3>`;
-   const ul=document.createElement('ul');
-   day.tasks.forEach((t,ti)=>{
-     const li=document.createElement('li');
-     const cb=document.createElement('input');
-     cb.type='checkbox';
-     const key=di+'-'+ti;
-     cb.checked=!!st[key];
-     cb.onchange=()=>{st[key]=cb.checked;localStorage.setItem('rk71',JSON.stringify(st));li.className=cb.checked?'done':'';}
-     li.className=cb.checked?'done':'';
-     li.append(cb,document.createTextNode(' '+t));
-     ul.appendChild(li);
-   });
-   div.appendChild(ul);
-   days.appendChild(div);
- });
- restaurants.innerHTML=d.restaurants.map(x=>'<li>'+x+'</li>').join('');
- pois.innerHTML=d.pois.map(x=>'<li>'+x+'</li>').join('');
+ const data=await (await fetch('assets/data/camping.json')).json();
+
+ facilities.innerHTML=data.voorzieningen.map(x=>'<li>'+x+'</li>').join('');
+
+ trips.innerHTML=data.uitstapjes.map(
+  x=>`<li><strong>${x.naam}</strong><br><a target="_blank" href="${x.maps}">Open in Apple Maps</a></li>`
+ ).join('');
 }
 window.onload=init;
