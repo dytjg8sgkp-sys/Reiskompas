7c38ec9169a4d66bcf43f5474af5d9eefcf3191c
dytjg8sgkp-sys <dytjg8sgkp@privaterelay.appleid.com>
2026-07-21 09:41:33 +0200

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,26 @@
 
-async function init(){
- const d=await (await fetch('assets/data/trip.json')).json();
- route.innerHTML=d.route.map(x=>'<li>'+x+'</li>').join('');
- days.innerHTML=d.days.map(x=>'<li>'+x+'</li>').join('');
- fav.innerHTML=d.favorites.map(x=>'<li>'+x+'</li>').join('');
+const items=[
+'Paspoorten controleren',
+'Caravan ophalen',
+'Hotel La Jonquera',
+'Vertrek naar Spanje',
+'Aankomst Playa Montroig'
+];
+function render(){
+ const ul=document.getElementById('checklist');
+ const state=JSON.parse(localStorage.getItem('rk61')||'[]');
+ ul.innerHTML='';
+ items.forEach((t,i)=>{
+   const li=document.createElement('li');
+   const cb=document.createElement('input');
+   cb.type='checkbox';
+   cb.checked=!!state[i];
+   cb.onchange=()=>{state[i]=cb.checked;localStorage.setItem('rk61',JSON.stringify(state));render();}
+   li.append(cb,document.createTextNode(' '+t));
+   if(cb.checked) li.className='done';
+   ul.appendChild(li);
+ });
+ const done=state.filter(Boolean).length;
+ document.getElementById('progress').textContent=`${done}/${items.length} afgerond`;
 }
-document.addEventListener('DOMContentLoaded',init);
+window.onload=render;
