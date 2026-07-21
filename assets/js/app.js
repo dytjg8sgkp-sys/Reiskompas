1ab719d826d00916f66a4de1225f2b18e7acecdb
dytjg8sgkp-sys <dytjg8sgkp@privaterelay.appleid.com>
2026-07-21 09:52:46 +0200

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,27 @@
 
 async function init(){
- const d=await (await fetch('assets/data/app.json')).json();
+ const d=await (await fetch('assets/data/data.json')).json();
+ const st=JSON.parse(localStorage.getItem('rk71')||'{}');
+ const days=document.getElementById('days');
+ d.days.forEach((day,di)=>{
+   const div=document.createElement('div');
+   div.innerHTML=`<h3>${day.title}</h3>`;
+   const ul=document.createElement('ul');
+   day.tasks.forEach((t,ti)=>{
+     const li=document.createElement('li');
+     const cb=document.createElement('input');
+     cb.type='checkbox';
+     const key=di+'-'+ti;
+     cb.checked=!!st[key];
+     cb.onchange=()=>{st[key]=cb.checked;localStorage.setItem('rk71',JSON.stringify(st));li.className=cb.checked?'done':'';}
+     li.className=cb.checked?'done':'';
+     li.append(cb,document.createTextNode(' '+t));
+     ul.appendChild(li);
+   });
+   div.appendChild(ul);
+   days.appendChild(div);
+ });
  restaurants.innerHTML=d.restaurants.map(x=>'<li>'+x+'</li>').join('');
- beaches.innerHTML=d.beaches.map(x=>'<li>'+x+'</li>').join('');
- itinerary.innerHTML=d.days.map(x=>`<tr><td>${x.day}</td><td>${x.plan}</td></tr>`).join('');
+ pois.innerHTML=d.pois.map(x=>'<li>'+x+'</li>').join('');
 }
 window.onload=init;
