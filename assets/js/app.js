45e23568ab57d064c7973d3cd2f947441782a8cf
dytjg8sgkp-sys <dytjg8sgkp@privaterelay.appleid.com>
2026-07-21 09:49:49 +0200

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,32 @@
 
-const packing=['Paspoorten','Rijbewijs','Caravanpapieren','Laders','Zwemkleding','EHBO-set'];
-function render(){
- const state=JSON.parse(localStorage.getItem('rk66_pack')||'[]');
- const ul=document.getElementById('packing');
- ul.innerHTML='';
- packing.forEach((item,i)=>{
+const activities=[
+'Inchecken camping',
+'Strand Playa Montroig',
+'Barcelona bezoeken',
+'PortAventura',
+'Cambrils boulevard',
+'Tarragona centrum'
+];
+
+function renderPlanner(){
+ const state=JSON.parse(localStorage.getItem('rk67_plan')||'[]');
+ const list=document.getElementById('planner');
+ list.innerHTML='';
+ activities.forEach((a,i)=>{
    const li=document.createElement('li');
    const cb=document.createElement('input');
    cb.type='checkbox';
    cb.checked=!!state[i];
-   cb.onchange=()=>{state[i]=cb.checked;localStorage.setItem('rk66_pack',JSON.stringify(state));render();}
-   li.append(cb,document.createTextNode(' '+item));
-   if(cb.checked)li.className='done';
-   ul.appendChild(li);
+   cb.onchange=()=>{
+      state[i]=cb.checked;
+      localStorage.setItem('rk67_plan',JSON.stringify(state));
+      renderPlanner();
+   };
+   li.append(cb,document.createTextNode(a));
+   if(cb.checked) li.className='done';
+   list.appendChild(li);
  });
- document.getElementById('progress').textContent=state.filter(Boolean).length+' / '+packing.length+' ingepakt';
+ document.getElementById('planprogress').textContent=
+   state.filter(Boolean).length+' / '+activities.length+' activiteiten voltooid';
 }
-window.onload=render;
+window.onload=renderPlanner;
