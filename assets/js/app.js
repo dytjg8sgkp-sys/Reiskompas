a80ea25e2acebdc2b601829b9ffc50e3cd0fa7a0
dytjg8sgkp-sys <dytjg8sgkp@privaterelay.appleid.com>
2026-07-21 09:51:58 +0200

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,8 @@
 
 async function init(){
- const data=await (await fetch('assets/data/search.json')).json();
- const list=document.getElementById('results');
- const search=document.getElementById('search');
- function render(filter=''){
-   list.innerHTML='';
-   data.filter(x=>x.toLowerCase().includes(filter.toLowerCase()))
-       .forEach(item=>{
-         const li=document.createElement('li');
-         li.textContent=item;
-         list.appendChild(li);
-       });
- }
- render();
- search.oninput=()=>render(search.value);
+ const d=await (await fetch('assets/data/app.json')).json();
+ restaurants.innerHTML=d.restaurants.map(x=>'<li>'+x+'</li>').join('');
+ beaches.innerHTML=d.beaches.map(x=>'<li>'+x+'</li>').join('');
+ itinerary.innerHTML=d.days.map(x=>`<tr><td>${x.day}</td><td>${x.plan}</td></tr>`).join('');
 }
 window.onload=init;
