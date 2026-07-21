29d93d49f218fed05ab303288ebb20258f4b1071
dytjg8sgkp-sys <dytjg8sgkp@privaterelay.appleid.com>
2026-07-21 08:27:57 +0200

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,1 @@
-const d=Math.max(0,Math.ceil((new Date('2026-07-18')-new Date())/86400000));document.getElementById('countdown').textContent='Nog '+d+' dagen';
+const d=Math.max(0,Math.ceil((new Date('2026-07-18')-new Date())/86400000));document.getElementById('count').textContent='Nog '+d+' dagen';
