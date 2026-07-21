765c8dc2dc904323c23c4aa6b4b1a5518ac806f1
dytjg8sgkp-sys <dytjg8sgkp@privaterelay.appleid.com>
2026-07-21 09:59:54 +0200

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,16 @@
-
 async function init(){
- const d=await (await fetch('assets/data/map.json')).json();
- const map=document.getElementById('campmap');
- d.cells.forEach(c=>{
-   const div=document.createElement('div');
-   div.className='cell';
-   div.textContent=c;
-   div.onclick=()=>alert(c);
-   map.appendChild(div);
- });
+const d=await (await fetch('assets/data/trip.json')).json();
+const b=d.booking;
+info.innerHTML=`<table>
+<tr><td>Camping</td><td>${b.camping}</td></tr>
+<tr><td>Type</td><td>${b.type}</td></tr>
+<tr><td>Aankomst</td><td>${b.arrival}</td></tr>
+<tr><td>Vertrek</td><td>${b.departure}</td></tr>
+<tr><td>Nachten</td><td>${b.nights}</td></tr>
+<tr><td>Referentie</td><td>${b.reference}</td></tr>
+<tr><td>Totaal</td><td>${b.price}</td></tr>
+<tr><td>Telefoon</td><td>${b.phone}</td></tr>
+</table>`;
+plan.innerHTML=d.planning.map(x=>`<tr><td>${x[0]}</td><td>${x[1]}</td></tr>`).join('');
 }
 window.onload=init;
